888338ab49c0d3caf9ba2106c4dc9708ec114da7
Edvard <edvard.jo@gmail.com>
2017-02-13 13:47:08 +0100

    geometry done

--- lab/main.m
+++ lab/main.m
@@ -1,3 +1,5 @@
+
+
 parameter1=1;
 parameter2=2;
 lambda_min=.1;
@@ -5,8 +7,3 @@ lambda_max=100;
 
 
 lambdastar = fzero(@(lambda) dphidlambda(lambda,parameter1,parameter2),[lambda_min lambda_max])
-
-
-
-
-
