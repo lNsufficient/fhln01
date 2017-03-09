dd565f5b9d032341ef770ff7652cfb17c1ffd950
Edvard <edvard.jo@gmail.com>
2017-03-09 18:26:28 +0100

    får itne plotstorlek att funkar

--- projekt/simp.m
+++ projekt/simp.m
@@ -57,12 +57,12 @@ else
 end
 
 E = 210*10^3*10^6; %Pa %THis was chosen by us, but is okay. Steel.
-nu = 0.27; %Steel, poissons ratio. 
-%E = 210*10^3; %Pa %THis was chosen by us, but is okay. 
+nu = 0.27; %Steel, poissons ratio.
+%E = 210*10^3; %Pa %THis was chosen by us, but is okay.
 D = E/(1-nu^2)*[1 nu 0; nu 1 0; 0 0 (1-nu)/2];
 %D = D(1:2,1:2);
 
-%% Set up the K matrix. 
+%% Set up the K matrix.
 ep = [1, t];
 
 xdim = nele;
@@ -75,21 +75,21 @@ elseif start_case == 3
     x = 0.01*ones(xdim,1);
 elseif start_case == 4
     load('simp_x_opt')
-    x = x_opt; 
+    x = x_opt;
 end
-    
-    
+
+
 K_all = cell(nele,1);
 for i = 1:nele
     K_all{i} = planqe(ex(i,:),ey(i,:),ep,D*1);
-
+    
 end
 
 K = getK_sheet(K_all, x, q, edof, nele, ndof);
 %
 
 % K = zeros(ndof);
-% 
+%
 % %Here, A = 1, so K = K0*A
 % ep = [ones(nele, 1)*E, ones(nele, 1)];
 % K_all = cell(nele,1);
@@ -98,7 +98,7 @@ K = getK_sheet(K_all, x, q, edof, nele, ndof);
 %     K_all{el} = Ke;
 %     %K(edof(el, 2:5), edof(el, 2:5)) = K(edof(el, 2:5), edof(el, 2:5)) + Ke;
 % end
-% 
+%
 
 %% Get the displacements
 
@@ -169,18 +169,18 @@ elseif filter_case == 2;
         phis = 3/(pi*R^2)*max(0, 1-dists/R);
         phis = phis(1:nele) + phis(nele+1:end);
         M(i,:) = phis'/sum(phis);
-
+        
         if mod(i,1e22) == 0
             figure(8);
             clf;
             fill(ex', ey', M(i,:))
             colorbar;
         end
-    end 
+    end
     M = sparse(M);
     x = M*x;
 elseif filter_case == 3
-    r = w*2;
+    r = w*1.4;
     ep_filt = [1, 3];
     
     %Not sure if this has to be done elementwise or if the following is
@@ -189,13 +189,13 @@ elseif filter_case == 3
     K_filt = zeros(nnod, nnod);
     M_filt = zeros(nnod, nnod);
     T_filt = zeros(nnod, nele);
-
+    
     for i = 1:nele
         ele_ind = enod(i,2:end); %This should be correct - rho only affects these indices.
         [K_filt_ele, T_filt_ele] = flw2i4e(ex(i,:), ey(i,:), ep_filt, eye(2), 1);
         T_filt(ele_ind,i) = T_filt_ele;
         K_filt(ele_ind, ele_ind) = K_filt(ele_ind, ele_ind) + K_filt_ele;
-        M_filt_ele = flw2i4m(ex(i,:),ey(i,:),1); 
+        M_filt_ele = flw2i4m(ex(i,:),ey(i,:),1);
         M_filt(ele_ind, ele_ind) = M_filt(ele_ind, ele_ind) + M_filt_ele;
     end
     
@@ -209,11 +209,11 @@ res = inf;
 
 while res > TOL
     
-%while nbr_runs < max_nbr_runs
-   nbr_runs = nbr_runs + 1;
-   %% Calculate the new K and corresponding u
-   tic
-   if filter_case == 3
+    %while nbr_runs < max_nbr_runs
+    nbr_runs = nbr_runs + 1;
+    %% Calculate the new K and corresponding u
+    tic
+    if filter_case == 3
         K = zeros(ndof, ndof);
         ep_K = [1,t,3];
         for i = 1:nele
@@ -221,66 +221,66 @@ while res > TOL
             edof_ele = edof(i, 2:end);
             K(edof_ele, edof_ele) = K(edof_ele, edof_ele) + Ke;
         end
-   else
+    else
         K = getK_sheet(K_all, x, q, edof, nele, ndof);
-   end
-   K = sparse(K);
-   times(nbr_runs,1) = toc;
-   
-   tic
-   u = solveq(K,F,bc);
-   times(nbr_runs,2) = toc;
-   %% Calculate derivatives
-   
-   tic
-   if (filter_case == 1 || filter_case ==2)
-       C = zeros(nele, 1);
-       for i = 1:nele
-           edof_ele = edof(i, 2:end);
-
-           u_ele = u(edof_ele);
-           Ke0 = K_all{i};
-           C(i) = (u_ele'*q*x(i)^(q-1)*Ke0*u_ele)/ae;
-       end
-       C = M'*C;
-   elseif filter_case == 3
-       dgdr = zeros(nnod, 1);
-       ep_dg = [1, t,3];
-       for i = 1:nele
-           ele_ind = enod(i, 2:end);
-           dgdr_ele = getdgdrhotilde_el(ex(i,:),ey(i,:),ep_dg,D,u(edof(i,2:end))',rho_tilde(ele_ind)',q);
-           dgdr(ele_ind) = dgdr(ele_ind) + dgdr_ele;
-       end
-       C = M'*dgdr/ae;
-   end
-      
-   times(nbr_runs,3) = toc;
-   
-   tic
-   lambdastar = fzero(@(lambda) dphidlambda(lambda, ae*ones(nele,1), C, x, x_max, x_min, V_max, alpha),[lambda_min lambda_max])
-   times(nbr_runs,4) = toc;
+    end
+    K = sparse(K);
+    times(nbr_runs,1) = toc;
+    
+    tic
+    u = solveq(K,F,bc);
+    times(nbr_runs,2) = toc;
+    %% Calculate derivatives
+    
+    tic
+    if (filter_case == 1 || filter_case ==2)
+        C = zeros(nele, 1);
+        for i = 1:nele
+            edof_ele = edof(i, 2:end);
+            
+            u_ele = u(edof_ele);
+            Ke0 = K_all{i};
+            C(i) = (u_ele'*q*x(i)^(q-1)*Ke0*u_ele)/ae;
+        end
+        C = M'*C;
+    elseif filter_case == 3
+        dgdr = zeros(nnod, 1);
+        ep_dg = [1, t,3];
+        for i = 1:nele
+            ele_ind = enod(i, 2:end);
+            dgdr_ele = getdgdrhotilde_el(ex(i,:),ey(i,:),ep_dg,D,u(edof(i,2:end))',rho_tilde(ele_ind)',q);
+            dgdr(ele_ind) = dgdr(ele_ind) + dgdr_ele;
+        end
+        C = M'*dgdr/ae;
+    end
+    
+    times(nbr_runs,3) = toc;
+    
+    tic
+    lambdastar = fzero(@(lambda) dphidlambda(lambda, ae*ones(nele,1), C, x, x_max, x_min, V_max, alpha),[lambda_min lambda_max])
+    times(nbr_runs,4) = toc;
     %% Get the new x
     x_old = x;
     tic
     [x, errors] = xstar(lambdastar, C, x, x_max, x_min, alpha);
     times(nbr_runs,5) = toc;
-%     if any(errors == 1)
-%         disp('hits the upper limits....');
-%     end
-%     if any(errors == -1)
-%         disp('hits the lower limits.....');
-%     end
-
-%     
-%     disp(sprintf('Current run was: %d', nbr_runs));
+    %     if any(errors == 1)
+    %         disp('hits the upper limits....');
+    %     end
+    %     if any(errors == -1)
+    %         disp('hits the lower limits.....');
+    %     end
+    
+    %
+    %     disp(sprintf('Current run was: %d', nbr_runs));
     %% Filter the design variables
     if filter_case == 3
         rho_tilde = M*x;
     else
         x = M*x; %rho_tilde could have been used in all cases, we guess.
     end
-
-
+    
+    
     res = norm(x-x_old,2);
     disp(res)
     Res = [Res; res];
@@ -297,49 +297,69 @@ end
 
 savestr = sprintf('%s_filter_%d_q_%d_alpha_%d_res_%1.2g_nbrRuns_%d', str, filter_case, q, alpha, Res(end), nbr_runs);
 p_h = 3.5
-paperInches = [0 0 4/3.5*p_h p_h];
+paperPpt= [345 550];
+figurePpt = round([100, 100 paperPpt*0.48]);
 
 
 %% Plot the displacements
 %Filter the optimal x
 
-%THE FOLLOWING LINE MIGHT BE NEEDED IN filter_case 1 AND 2. 
+%THE FOLLOWING LINE MIGHT BE NEEDED IN filter_case 1 AND 2.
 %IT IS ALWAYS NEEDED MAYBE!!?!?
-x = M*x;
-if filter_case == 3
-    x = extract(enod, x);
-end
+rho_tilde = M*x;
+
 
 x_opt = x;
 if filter_case == 3
-    K = K; %WE SHOULD RECALCULATE THE K MATRIX HERE WHEN EVERYTHING WORKS!
+    K = zeros(ndof, ndof);
+    ep_K = [1,t,3];
+    for i = 1:nele
+        Ke = plani4e_rho(ex(i,:),ey(i,:),ep_K,D,rho_tilde(enod(i,2:end))',q);
+        edof_ele = edof(i, 2:end);
+        K(edof_ele, edof_ele) = K(edof_ele, edof_ele) + Ke;
+    end %WE SHOULD RECALCULATE THE K MATRIX HERE WHEN EVERYTHING WORKS!
 else
     K = getK_sheet(K_all, x, q, edof, nele, ndof);
 end
+
+if filter_case == 3
+    rho_tilde = extract(enod, rho_tilde);
+end
+
 u = solveq(K, F, bc);
 ed = extract(edof,u);
 fac = 100000;
-
+%%
 gcf = figure(3);
 
 clf;
 
 eldisp2(ex,ey,ed,plotpar,fac)
 %title(sprintf('Deformation of optimized structure,\n magnification factor %d',fac), 'interpreter', 'latex');
-set(gcf,'PaperUnits','inches','PaperSize',paperInches(3:4),'PaperPosition',paperInches)
-print('-dpng','-r100','test')
+%set(gcf,'PaperUnits','points','PaperSize',figurePpt(3:4),'PaperPosition',figurePpt)
+set(gcf,'Position',figurePpt)
+print('-dpng','test')
 
 %%
-figure(4);
+gcf = figure(4);
 clf;
 
-% 
-fill([ex' -ex'], [ey' ey'], [x' x'], 'linestyle', 'none')
+%
+fill([ex' -ex'], [ey' ey'], [rho_tilde' rho_tilde'], 'linestyle', 'none')
+colorbar;
+set(gcf,'Position',figurePpt)
+print('-dpng','test')
+
+%%
+figure(5);
+clf;
+
+%
+fill([ex' -ex'], [ey' ey'], [x; x], 'linestyle', 'none')
 colorbar;
 
-
 %% Plot the residual
-figure(5);
+figure(6);
 clf;
 % subplot(2,1,1)
 % plot(Res);
@@ -348,14 +368,14 @@ semilogy(Res)
 xlabel('Iteration number');
 
 %%
-figure(6)
+figure(7)
 clf
 hist(x);
 save_str = sprintf('coarse_%d_TOL_%2.2g_alpha_%d', load_coarse, TOL, alpha)
 savefig(sprintf('%s.fig',save_str))
 
 %%
-figure(7)
+figure(8)
 
 subplot(1, 2, 1)
 plot(G0)
